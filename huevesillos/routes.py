658238463862851e1d6102cce388571e6196431e
linuxnewbie84 <rebeto06@gmail.com>
2023-09-12 16:16:21 -0600

    Cambios en Flutter

--- huevesillos/routes.py
+++ huevesillos/routes.py
@@ -2,20 +2,20 @@ from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import HTMLResponse
 from uuid import uuid4
 from trataima import img, imgw
+import os
 
-
-ruta = "huevesillos/imagenesr/"
+rute = "huevesillos/imagenesr/"
 
 router = APIRouter()
 @router.post("/subir/", tags=["Subida de Archivos"])
 async def upload_huevos(file:UploadFile =File(...)):
     file.filename = f"{uuid4()}.jpg"
     recibido = await file.read()
-    with open(f"{ruta}{file.filename}", "wb") as huevesillor:
+    with open(f"{rute}{file.filename}", "wb") as huevesillor:
         huevesillor.write(recibido)
         huevesillor.close()
-        l = img(ruta + file.filename)
-        l2 = imgw(ruta+ file.filename)   
+        l = img(rute + file.filename)
+        l2 = imgw(rute+ file.filename)   
     return l.trar()
 #Se termina el tratamiento de imagenes y se guardan
    
--- huevos/lib/home_page.dart
+++ huevos/lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:huevos/acciones/camara.dart';
 import 'package:huevos/acciones/selec.dart';
-import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -21,9 +21,25 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Ovitraap Menú'),
+        title: Row(mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+              Image.asset(
+                'assets/images/logo.png',
+                scale: 12,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'Ovitraapp',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        backgroundColor: Colors.deepOrangeAccent,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           sele_img != null
               ? Image.file(sele_img!)
@@ -31,27 +47,81 @@ class _HomePageState extends State<HomePage> {
                   margin: const EdgeInsets.all(20),
                   height: 200,
                   width: double.infinity,
-                  color: Colors.black,
+                  color: Color.fromARGB(179, 54, 54, 54),
                 ),
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red, foregroundColor: Colors.yellow),
               onPressed: () async {
                 final cama = await getImageC();
-                if (cama == null) return;
-                setState(() {
-                  sele_img = File(cama.path);
-                });
+                if (cama != null) {
+                  CroppedFile? croppedFile = await ImageCropper().cropImage(
+                    sourcePath: cama.path,
+                    aspectRatioPresets: [
+                      CropAspectRatioPreset.square,
+                      CropAspectRatioPreset.ratio3x2,
+                      CropAspectRatioPreset.original,
+                      CropAspectRatioPreset.ratio4x3,
+                      CropAspectRatioPreset.ratio16x9
+                    ],
+                    uiSettings: [
+                      AndroidUiSettings(
+                          toolbarTitle: 'Dimensionando para Análisis',
+                          toolbarColor: Colors.deepOrange,
+                          toolbarWidgetColor: Colors.white,
+                          initAspectRatio: CropAspectRatioPreset.original,
+                          lockAspectRatio: false),
+                      IOSUiSettings(
+                        title: 'Dimensión de Imagen',
+                      ),
+                    ],
+                  );
+                  if (croppedFile != null) {
+                    setState(() {
+                      sele_img = File(croppedFile.path);
+                    });
+                  }
+                }
               },
-              child: const Text("Tomar Foto")),
+              child: const Text("Tomar Foto",)),
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red, foregroundColor: Colors.yellow),
               onPressed: () async {
                 final simagen = await getImage();
-                if (simagen == null) return;
-                setState(() {
-                  sele_img = File(simagen.path);
-                });
+                if (simagen != null) {
+                  CroppedFile? croppedFile = await ImageCropper().cropImage(
+                    sourcePath: simagen.path,
+                    aspectRatioPresets: [
+                      CropAspectRatioPreset.square,
+                      CropAspectRatioPreset.ratio3x2,
+                      CropAspectRatioPreset.original,
+                      CropAspectRatioPreset.ratio4x3,
+                      CropAspectRatioPreset.ratio16x9
+                    ],
+                    uiSettings: [
+                      AndroidUiSettings(
+                          toolbarTitle: 'Dimensionando para Análisis',
+                          toolbarColor: Colors.deepOrange,
+                          toolbarWidgetColor: Colors.white,
+                          initAspectRatio: CropAspectRatioPreset.original,
+                          lockAspectRatio: false),
+                      IOSUiSettings(
+                        title: 'Dimensión de Imagen',
+                      ),
+                    ],
+                  );
+                  if (croppedFile != null) {
+                    setState(() {
+                      sele_img = File(croppedFile.path);
+                    });
+                  }
+                }
               },
               child: const Text("Seleccionar Imagen")),
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red, foregroundColor: Colors.yellow),
               onPressed: () {},
               child: const Text("Subir Imagen para su Análisis"))
         ],
